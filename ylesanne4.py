e9e129f2a52080e851f17d36d2bc6319f1e891b3
Martin-Tehvanus <martin.tehvanus@vikk.ee>
2025-03-17 22:43:42 +0200

    Ülesanne 4, täiendatud, peab veel üle vaatama

--- ylesanne4.py
+++ ylesanne4.py
@@ -7,14 +7,19 @@ pygame.init()
 # muutujad
 clock = pygame.time.Clock()
 skoor = 0
-korgus1 = random.randrange(0, 479)
-korgus2 = random.randrange(0, 479)
-kiirus = 5
+kiirus = 3 # auto liikumise kiirus
+
+# auto alustuse kõrgused (suvaliselt valitud 1 ja 479 vahel)
+korgus1 = random.randrange(1, 479)
+korgus2 = random.randrange(1, 479)
+korgus3 = random.randrange(1, 479)
 
 
-# auto lubatud asukohad
-alg1 = 180, korgus1 # esimene tee vahemik, esimene arv paneb auto tee keskele, teiseks on kõrgus
-alg2 = 420, korgus2 # teine tee vahemik, esimene arv paneb auto tee keskele, teiseks on kõrgus
+
+# auto tee vahemikud (paneb iga auto teatud tee rea peale)
+alg1 = 180, korgus1 # esimene tee rida, esimene arv paneb auto tee keskele (joonte vahele), teiseks on kõrgus
+alg2 = 300, korgus2
+alg3 = 420, korgus3
 
 # aken
 screen=pygame.display.set_mode([640,480])
@@ -23,48 +28,52 @@ pygame.display.set_caption("Ülesanne 4")
 
 # autod ja taust
 punane_auto = pygame.image.load("yl4/f1_red.png")
-sinine_auto1 = pygame.image.load("yl4/f1_blue.png")
-sinine_auto2 = pygame.image.load("yl4/f1_blue.png")
+sinine_auto1 = pygame.image.load("yl4/f1_blue.png") # vasak
+sinine_auto2 = pygame.image.load("yl4/f1_blue.png") # keskmine
+sinine_auto3 = pygame.image.load("yl4/f1_blue.png") # parem
 taust = pygame.image.load("yl4/bg_rally.jpg")
 
 # põhikood
-
-
-
-
-
 while True:
-    clock.tick(60)
-# asjade kuvamine
+    clock.tick(60) # fps (60+ sobilik)
+
+    # autode liikumine
+    korgus1 += kiirus
+    korgus2 += kiirus
+    korgus3 += kiirus
+
+    # alla sõit + skoori lisamine
+    if korgus1 > 480:
+        korgus1 = 0
+        skoor +=1
+    if korgus2 > 480:
+        korgus2 = 0
+        skoor +=1
+    if korgus3 > 480:
+        korgus3 = 0
+        skoor +=1
+
+    # autode positiooni uuendamine (pärast alla jõudmist ja skoori saamist)
+    alg1 = (180, korgus1)
+    alg2 = (300, korgus2)
+    alg3 = (420, korgus3)
+
+    # asjade kuvamine pygame aknasse
     screen.blit(taust, (0,0))
     screen.blit(punane_auto, (300,390))
     screen.blit(sinine_auto1, alg1)
     screen.blit(sinine_auto2, alg2)
+    screen.blit(sinine_auto3, alg3)
 
-
-#skoor
+    #skoori kuvmine pygame aknasse
     font = pygame.font.Font(None, 35)
-    text = font.render(str(skoor), True, [255, 255, 255])
-    screen.blit(text, (310, 0))
+    text = font.render("Skoor: " + str(skoor), True, [255, 255, 255])
+    screen.blit(text, (270, 0))
 
-# ristist kinni
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        break
+# ristist kinni panemine
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
     pygame.display.flip()
-pygame.quit()
-
-
-
-
-# Puudu
-
-#Lisa siniste autode animatsioon ülevalt alla
-#   autod jäävad tee vahemikku - tehtud
-#   alustavad erinevatelt kõrgustelt - tehtud
-#   kui auto alla jõuab, hakkab ta uuesti ülevalt alla liikuma
-
-#Kuva skoor - tehtud
-#   kui sinine auto jõuab alla, lisatakse skoorile punkte juurde
-#   arv, mida soovid lisada tekstikasti, tuleb teisendada tekstiks str(number) - tehtud
