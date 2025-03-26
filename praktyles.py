80768ce9203e1b7c1e115f829f1f368278a630c7
martin.tehvanus <martin.tehvanus@vikk.ee>
2025-03-26 15:17:05 +0200

    Prakt ülesanne valmis

--- praktyles.py
+++ praktyles.py
@@ -1,68 +1,86 @@
 import pygame
-import sys
 import random
+import sys
+import time
 
 pygame.init()
 
-screen=pygame.display.set_mode([1080,720])
-pygame.display.set_caption("Praktiline ülesanne")
-screen.fill([123,123,123])
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Collision")
 clock = pygame.time.Clock()
 
-posX = 1080/2
-posY = 720/2
-kiirusX = 0
-kiirusY = 0
-suundX = 0
-suundY = 0
+#create main rectangle
+rect_1 = pygame.Rect(0, 0, 25, 25)
 
-rstX = 0
-rstY = 0
+posX, posY = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
+speedX, speedY = 0, 0
+directionX, directionY = 0, 0
 
-ristkylikud = []
-for i in range(15):
-    rstX = random.randint(1, 1080)
-    rstY = random.randint(1, 720)
-    ristkylikud.append([rstX, rstY])
+#create empty list, then create 16 obstacle rectangles using a loop and add to list
+obstacles = []
+for _ in range(16):
+  obstacle_rect = pygame.Rect(random.randint(0, 500), random.randint(0, 300), 25, 25)
+  obstacles.append(obstacle_rect)
 
-    for i in range(len(ristkylikud)):
-        pygame.draw.rect(screen, [255, 0, 0], [ristkylikud[i][0], ristkylikud[i][1], 20, 20])
+#define colours
+BG = (50, 50, 50)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
-while True:
+
+run = True
+while run:
     clock.tick(60)
+  #update background
+    screen.fill(BG)
+
+  #check collision and change colour
+    col = GREEN
+    for obstacle in obstacles:
+        if rect_1.colliderect(obstacle):
+            col = RED
+
+
+  #draw all rectangles
+    pygame.draw.rect(screen, col, rect_1)
+    for obstacle in obstacles:
+        pygame.draw.rect(screen, BLACK, obstacle,)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            run = False
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                kiirusX = 4
+                speedX = 3
             elif event.key == pygame.K_LEFT:
-                kiirusX = -4
+                speedX = -3
             elif event.key == pygame.K_UP:
-                kiirusY = -4
+                speedY = -3
             elif event.key == pygame.K_DOWN:
-                kiirusY = 4
+                speedY = 3
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                kiirusX = 0
+                speedX = 0
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                kiirusY = 0
+                speedY = 0
 
-    for i in range(len(ristkylikud)):
-        pygame.draw.rect(screen, [0, 0, 0], [ristkylikud[i][0], ristkylikud[i][1], 40, 50])
-
-
-    posX += kiirusX
-    posY += kiirusY
-
-
-    ruut = pygame.draw.rect(screen,[0, 0, 255],[posX, posY, 30, 30])
-
-     if ruut.colliderect(ristkylikud):
-        print("Töötab")
-
+    posX += speedX
+    posY += speedY
+    rect_1 = pygame.draw.rect(screen, col, [posX, posY, 30, 30])
+  #update display
     pygame.display.flip()
-    screen.fill([123,123,123])
+    screen.fill(BLUE)
+
+    if col == RED:
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
+pygame.quit()
